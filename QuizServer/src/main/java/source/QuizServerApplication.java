571c3bd47bc98c79886b4434fb5efd6fd85fe0e0
esurajpandey <esurajpandey@gmail.com>
2024-02-24 21:25:36 +0530

    Backend server setup done

---
+++ QuizServer/src/main/java/source/QuizServerApplication.java
@@ -0,0 +1,13 @@
+package source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServerApplication.class, args);
+	}
+
+}
---
+++ QuizServer/src/test/java/source/QuizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
